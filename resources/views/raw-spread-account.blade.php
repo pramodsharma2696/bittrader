34a8eae0d69cdc602d043dc1289f4f82ba48eedd
pramodsharma <pramodsharma263@gmail.com>
2024-10-22 13:43:38 +0530

    commit it

--- resources/views/raw-spread-account.blade.php
+++ resources/views/raw-spread-account.blade.php
@@ -7,12 +7,12 @@
     <div class="background bg--cover" style="background-image:url(assets/images/header/1.png);padding: 100px 20px;">
         <div class="container">
             <div class="page-header__content" data-aos="fade-right" data-aos-duration="1000">
-                <h2>RAW Spread Account</h2>
+                <h2>Raw Spread Account</h2>
                 <nav style="--bs-breadcrumb-divider: '/';" aria-label="breadcrumb" class="d-none">
                     <ol class="breadcrumb mb-0">
                         <li class="breadcrumb-item "><a href="{{ url('/') }}">Home</a></li>
                         <li class="breadcrumb-item" aria-current="page"><a href="{{ url('/account-Types') }}">Account Type</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">RAW Spread Account</li>
+                        <li class="breadcrumb-item active" aria-current="page">Raw Spread Account</li>
                     </ol>
                 </nav>
             </div>
@@ -29,34 +29,14 @@
 <div class="service-details padding-bottom section-bg-color" style="margin-top:30px;">
     <div class="container">
         <div class="service-details__wrapper">
-            <div class="row flex-lg-row-reverse g-5">
-                <div class="col-lg-12">
-                    <div class="service-details__item">
-                        <div class="service-details__item-inner ">
-
-                            <div class="service-details__content" data-aos="fade-up" data-aos-duration="900">
-                                <h3 class="mb-15"> <a class="stretched-link" href="javascript:void(0);">RAW Spread Account</a>
-                                </h3>
-                                <p class="mb-0">For traders looking to access truly tight spreads - FX majors as low as 0.0 - with low commissions.</p>
-                            </div>
-
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-           
-
-            <!-- 2 -->
-           
-            <!-- 3 -->
+ 
             <div class="row">
                 <div class="col-lg-6">
                     <div class="service-details__item">
                         <div class="service-details__item-inner ">
                             <div class="service-details__thumb mb-40" data-aos="fade-up" data-aos-duration="800">
                                 <div class="service-details__thumb-inner">
-                                    <img src="{{ asset('') }}assets/images/others/raw-pricing-platform-image.jpeg" alt="service-icon">
+                                    <img src="{{ asset('') }}assets/images/others2/1.png" alt="service-icon">
                                 </div>
                             </div>
                         </div>
@@ -66,15 +46,21 @@
                     <div class="service-details__item">
                         <div class="service-details__item-inner ">
                             <div class="service-details__content" data-aos="fade-up" data-aos-duration="900">
-                                <h3 class="mb-15"> <a class="stretched-link" href="javascript:void(0);">Trade the global currency markets your way</a>
+                                <h3 class="mb-15"> <a class="stretched-link" href="javascript:void(0);">Raw Spread Account</a>
                                 </h3>
-                                <p class="mb-3">Qovra Markets suite of powerful trading platforms helps traders gain an edge on the currency markets.</p>
-                                <h5>Get informed and prepared</h5>
-                                <p class="mb-2">Use integrated trading tools, market commentary, and expert analysis to gain an edge.</p>
-                                <h5>Visualize the trade</h5>
-                                <p class="mb-2">Fine-tune your capabilities with our advanced charting tools and 80+ technical indicators.</p>
-                                <h5>Mobile optimized</h5>
-                                <p class="mb-2">Our powerful mobile app delivers a complete trading experience, right at your fingertips.</p>
+                                <p class="mb-3">The Raw Account at Qorva Markets caters to traders who prioritize cost-efficiency and direct market access. It offers raw spreads starting from 0 pips, with no mark-ups, ensuring transparent pricing and optimal trading conditions. This account is ideal for high-volume traders seeking to maintain maximum control over their trading costs.</p>
+                                <h5>Benefits</h5>
+                                <p class="mb-2"><b>.</b>Commission $6 per lot</p>
+                                <p class="mb-2"><b>.</b> No minimum deposit</p>
+                                <p class="mb-2"><b>.</b> Leverage up to 1:100</p>
+                                <h5>Currencies</h5>
+                                <p class="mb-2"><b>.</b>Commission $6 per lot</p>
+                                <p class="mb-2"><b>.</b> No minimum deposit</p>
+                                <p class="mb-2"><b>.</b> Leverage up to 1:100</p>
+                                <h5>Other Offers</h5>
+                                <p class="mb-2"><b>.</b>Commission $6 per lot</p>
+                                <p class="mb-2"><b>.</b> No minimum deposit</p>
+                                <p class="mb-2"><b>.</b> Leverage up to 1:100</p>
                             </div>
                         </div>
                     </div>
@@ -82,6 +68,57 @@
 
 
             </div>
+
+            <div class="row flex-lg-row-reverse g-5">
+                <div class="col-lg-12">
+                    <div class="service-details__item">
+                        <div class="service-details__item-inner ">
+
+                            <div class="service-details__content" data-aos="fade-up" data-aos-duration="900">
+                                <h3 class="mb-15"> <a class="stretched-link" href="javascript:void(0);">Raw Spread Account Info</a>
+                                </h3>
+                                <p class="mb-0">Bound to ensue and equal blame belongs to those who fail in their</p>
+                                <ul class="list-group">
+                                    <li class="list-group-item">Initial Deposit 5100</li>
+                                    <li class="list-group-item">Leverage Upto 1:100</li>
+                                    <li class="list-group-item">Order Volume 0.01 – 500 lots</li>
+                                    <li class="list-group-item">SpreadFixed from 0 pis</li>
+                                    <li class="list-group-item">24/7 SupportYes</li>
+                                    <li class="list-group-item">Multi Language CCYes</li>
+                                </ul>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row flex-lg-row-reverse g-5">
+                <div class="col-lg-12">
+                    <div class="service-details__item">
+                        <div class="service-details__item-inner ">
+
+                            <div class="service-details__content" data-aos="fade-up" data-aos-duration="900">
+                                <h3 class="mb-15"> <a class="stretched-link" href="javascript:void(0);">Reason for Choose</a>
+                                </h3>
+                                <p class="mb-0">Bound to ensue and equal blame belongs to those who fail in their</p>
+                                <h5>Friendly & Expert01</h5>
+                                <p class="mb-2">Enjoy a friendly and expert team dedicated to your trading success, providing personalized support and expert guidance every step of the way.</p>
+                                <h5>24/7 Support02</h5>
+                                <p class="mb-2">Experience round-the-clock support at Qorva Markets, ensuring assistance and guidance whenever you need it to enhance your trading journey</p>
+                                <h5>Social Trading</h5>
+                                <p class="mb-2">With our copy trading feature, effortlessly replicate the trades of seasoned investors, leveraging their expertise to optimize your own portfolio.</p>
+                                <h5>IB Rewards</h5>
+                                <p class="mb-2">With our IB Rewards program, benefit from lucrative incentives and bonuses as an Introducing Broker, unlocking exclusive perks and rewards for your successful referrals.</p>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+
         </div>
     </div>
 </div>
